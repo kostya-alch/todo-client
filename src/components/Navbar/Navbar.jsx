0b31c5e1f1347b5c6f1e42d025161e954ae16d95
kostya-alch <kostafrompiter@gmail.com>
2022-03-14 15:25:47 +0300

    authform and reg form

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
         <header>
             <nav>
                 <div className="nav-wrapper navbar">
-                    <a href="/" className="brand-logo center">Список дел</a>
-                    <ul id="nav-mobile" className="left hide-on-med-and-down">
+                    <a href="/" className="brand-logo left">Список дел</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a className='hoverable link' href="/">Войти в аккаунт</a></li>
                     </ul>
                 </div>
--- src/pages/Auth/AuthPage.jsx
+++ src/pages/Auth/AuthPage.jsx
@@ -28,12 +28,41 @@ const AuthPage = () => {
                             </div>
                         </div>
                         <div className="row">
-                            <button className='wawes-effect wawes-light btn btn-blue'>
+                            <button className='wawes-effect wawes-light btn btn-blue hoverable '>
                                 Войти в аккаунт
                             </button>
                             <a href="/" className="btn-outline btn-reg">Нет аккаунта в приложении?</a>
                         </div>
                     </form>
+
+                    <h3>Регистрация</h3>
+                    <form className='form form-login'>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input
+                                    type="email"
+                                    name='email'
+                                    className='validate'
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    type="password"
+                                    name='password'
+                                    className='validate'
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <button className='wawes-effect wawes-light btn btn-blue hoverable '>
+                                Регистрация на сайте
+                            </button>
+                            <a href="/" className="btn-outline btn-reg">Уже есть аккаунт?</a>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </>
