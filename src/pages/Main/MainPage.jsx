0e7662e53de32413794cfeda72bb28c1f52d1bf0
kostya-alch <kostafrompiter@gmail.com>
2022-03-18 21:41:13 +0300

    logic add todo view, logic remove todo

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import { AuthContext } from '../../context/authContext'
 
@@ -15,23 +15,51 @@ const MainPage = () => {
     const inputHandler = (event) => {
         setText(event.target.value)
     }
-    const createTodo = useCallback(async () => {
-        if (!text) {
-            return null;
+    const getTodo = useCallback(async () => {
+        try {
+            await axios.get('/api/todo', {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                params: { userId }
+            }).then(response => setTodos(response.data))
+        } catch (error) {
+            console.log(error);
         }
+    }, [userId])
+
+    const createTodo = useCallback(async () => {
+        if (!text) return null;
         try {
             await axios.post('/api/todo/add', { text, userId }, {
                 headers: {
                     'Content-Type': 'application/json'
                 }
             }).then(response => {
-                setTodos([...todos], response.data)
-                setText('')
+                setTodos([...todos], response.data);
+                setText('');
+                getTodo();
             })
         } catch (error) {
             console.log(error);
         }
-    }, [text, userId, todos])
+    }, [text, userId, todos, getTodo])
+
+    useEffect(() => {
+        getTodo()
+    }, [getTodo])
+
+    const removeTodo = useCallback(async (id) => {
+        try {
+            await axios.delete(`/api/todo/delete/${id}`, { id }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(() => getTodo())
+        } catch (error) {
+            console.log(error);
+        }
+    }, [getTodo])
     return (
         <>
             <div className="container">
@@ -59,15 +87,21 @@ const MainPage = () => {
 
                     <h3>Активные задачи:</h3>
                     <div className='todos'>
-                        <div className="row flex todos-item">
-                            <div className="col todos-num">1</div>
-                            <div className="col todos-text">Text</div>
-                            <div className="col todos-buttons">
-                                <i className="material-icons blue-text">add</i>
-                                <i className="material-icons orange-text">warning</i>
-                                <i className="material-icons red-text">delete</i>
-                            </div>
-                        </div>
+                        {
+                            todos.map((todo, index) => {
+                                return (
+                                    <div className="row flex todos-item" key={index}>
+                                        <div className="col todos-num">{index + 1}</div>
+                                        <div className="col todos-text">{todo.text}</div>
+                                        <div className="col todos-buttons">
+                                            <i className="material-icons blue-text">add</i>
+                                            <i className="material-icons orange-text">warning</i>
+                                            <i onClick={() => removeTodo(todo._id)} className="material-icons red-text">delete</i>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
