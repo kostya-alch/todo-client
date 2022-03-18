fe7ed03b70b85f12eb34dccd1e8c15c4d493d0c8
kostya-alch <kostafrompiter@gmail.com>
2022-03-18 16:07:21 +0300

    logic add todo

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
+import React, { useCallback, useContext, useState } from 'react'
+import axios from 'axios'
+import { AuthContext } from '../../context/authContext'
 
 import './MainPage.scss'
 
 const MainPage = () => {
+    const [text, setText] = useState('')
+    const [todos, setTodos] = useState([])
+    const { userId } = useContext(AuthContext)
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    }
+    const inputHandler = (event) => {
+        setText(event.target.value)
+    }
+    const createTodo = useCallback(async () => {
+        if (!text) {
+            return null;
+        }
+        try {
+            await axios.post('/api/todo/add', { text, userId }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                setTodos([...todos], response.data)
+                setText('')
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }, [text, userId, todos])
     return (
         <>
             <div className="container">
                 <div className="main-page">
                     <h4>Добавить задачу:</h4>
-                    <form className='form form-login'>
+                    <form className='form form-login' onSubmit={submitHandler}>
                         <div className="row">
                             <div className="input-field col s12">
                                 <input type="text"
                                     id='text'
                                     name='input-todo'
-                                    className='validate' />
+                                    className='validate'
+                                    onChange={inputHandler}
+                                    value={text}
+                                />
                                 <label htmlFor="input-todo">Задача: </label>
                             </div>
                         </div>
                         <div className="row">
-                            <button className='wawes-effect wawes-light btn blue'>
+                            <button onClick={createTodo} className='waves-effect waves-light btn blue'>
                                 Добавить задачу
                             </button>
                         </div>
@@ -31,9 +63,9 @@ const MainPage = () => {
                             <div className="col todos-num">1</div>
                             <div className="col todos-text">Text</div>
                             <div className="col todos-buttons">
-                                <i class="material-icons blue-text">add</i>
-                                <i class="material-icons orange-text">warning</i>
-                                <i class="material-icons red-text">delete</i>
+                                <i className="material-icons blue-text">add</i>
+                                <i className="material-icons orange-text">warning</i>
+                                <i className="material-icons red-text">delete</i>
                             </div>
                         </div>
                     </div>
